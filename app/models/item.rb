d563650b04666bd612a2742b1224fb5accc7f62d
Motoki22 <mo.sato.nmnl@gmail.com>
2023-09-10 20:37:39 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,10 @@ class Item < ApplicationRecord
   
   belongs_to :user
   has_one_attached :image
+  validates :image, presence: true
+
+  validates :item_name, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true }, format: { with: /\A[0-9]+\z/}
+  validates :explain, presence: true
+  validates :category_id, :condition_id, :fee_status_id, :prefecture_id, :delivery_schedule_id, presence: true, numericality: { other_than: 1 } 
 end
