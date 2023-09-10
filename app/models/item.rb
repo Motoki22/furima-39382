91780b70b487d0d112e4bf50f53bebb027e7f895
Motoki22 <mo.sato.nmnl@gmail.com>
2023-09-10 18:25:50 +0900

    ActiveRecord,ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :fee_status
+  belongs_to :prefecture
+  belongs_to :delivery_schedule
+  
   belongs_to :user
+  has_one_attached :image
 end
