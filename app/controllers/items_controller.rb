070e8f78398c0d566ea9844ea1384869371da1ba
Motoki22 <mo.sato.nmnl@gmail.com>
2023-09-11 22:15:08 +0900

    出品ページを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,22 @@
 class ItemsController < ApplicationController
-  def index
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new # バリデーションエラー時には再び出品ページを表示します。
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :price, :explain, :category_id, :condition_id, :fee_status_id, :prefecture_id, :delivery_schedule_id).merge(user_id: current_user.id)
   end
-
 end
