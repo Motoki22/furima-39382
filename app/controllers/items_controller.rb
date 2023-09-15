0c946580ff33ae881c79c932e1b0485137249079
Motoki22 <mo.sato.nmnl@gmail.com>
2023-09-15 18:13:21 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,9 +18,9 @@ class ItemsController < ApplicationController
     @items = Item.order(created_at: :desc)
   end
 
-  def show
-    @item = Item.find(params[:id])
-  end
+  # def show
+  #   @item = Item.find(params[:id])
+  # end
 
   private
 
