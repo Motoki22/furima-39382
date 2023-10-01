2aff587e1bc9f3af022a52e5a1c3bd807948269c
Motoki22 <mo.sato.nmnl@gmail.com>
2023-10-01 22:33:00 +0900

    テストコード　情報の保存まで

--- spec/models/purchase_shipping_address_spec.rb
+++ spec/models/purchase_shipping_address_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseShippingAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @purchase_shipping_address = FactoryBot.build(:purchase_shipping_address, user_id: user.id, item_id: item.id)
+  end
+
+  describe '商品購入機能' do
+    context '購入ができる場合' do
+      it '全ての情報が正しく入力されていれば保存できること' do
+        expect(@purchase_shipping_address).to be_valid
+      end
+
+      it 'buildingは空でも保存できること' do
+        @purchase_shipping_address.building = ''
+        expect(@purchase_shipping_address).to be_valid
+      end
+    end
+
+    context '購入ができない場合' do
+      it 'postal_codeが空だと保存できないこと' do
+        @purchase_shipping_address.postal_code = nil
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+
+      it 'postal_codeが3桁ハイフン4桁の半角文字列でないと保存できないこと' do
+        @purchase_shipping_address.postal_code = '1234567'
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+      end
+
+      it 'prefecture_idが空だと保存できないこと' do
+        @purchase_shipping_address.prefecture_id = 0
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it 'cityが空だと保存できないこと' do
+        @purchase_shipping_address.city = ''
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("City can't be blank")
+      end
+
+      it 'addressが空だと保存できないこと' do
+        @purchase_shipping_address.address = ''
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Address can't be blank")
+      end
+
+      it 'phone_numberが空だと保存できないこと' do
+        @purchase_shipping_address.phone_number = ''
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      it 'phone_numberが10桁以上11桁以内の半角数値でないと保存できないこと' do
+        @purchase_shipping_address.phone_number = '090-1234-5678'
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Phone number is invalid. Input only number")
+      end
+
+      it 'phone_numberが9桁以下だと保存できないこと' do
+        @purchase_shipping_address.phone_number = '090123456'
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Phone number is too short")
+      end
+    end
+  end
 end
