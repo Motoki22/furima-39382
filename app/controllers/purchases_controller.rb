3734dacf1ad8942a52aac8c3223eca91b5f26deb
Motoki22 <mo.sato.nmnl@gmail.com>
2023-10-15 22:50:11 +0900

    追加実装（ユーザー関連）

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,7 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+  before_action :redirect_conditions, only: [:index, :create]
 
   def index
     @purchase_shipping_address = PurchaseShippingAddress.new
@@ -25,6 +27,12 @@ class PurchasesController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def redirect_conditions
+    if @item.user_id == current_user.id || (@item.purchase.present? && @item.user_id != current_user.id)
+      redirect_to root_path
+    end
   end
 
   def purchase_params
