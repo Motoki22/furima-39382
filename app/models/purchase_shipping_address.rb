f165ca6e417c2cd95255b531b59ae6b5e4e2aaa5
Motoki22 <mo.sato.nmnl@gmail.com>
2023-10-18 12:13:59 +0900

    バリデーション修正

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -6,7 +6,7 @@ class PurchaseShippingAddress
     validates :user_id
     validates :item_id
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
     validates :phone_number, length: { in: 10..11, message: 'is too short' },
